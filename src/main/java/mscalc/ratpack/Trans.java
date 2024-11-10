4c202054c0c148efc0986e4878e8e753c1373ec0
Marcin Chwedczuk <0xmarcin+gh@gmail.com>
2024-11-10 17:33:50 +0100

    add cos and tan

--- src/main/java/mscalc/ratpack/Trans.java
+++ src/main/java/mscalc/ratpack/Trans.java
@@ -1,11 +1,15 @@
 package mscalc.ratpack;
 
+import mscalc.cpp.ErrorCodeException;
 import mscalc.cpp.Ptr;
 import mscalc.cpp.uint;
 import mscalc.ratpack.RatPack.AngleType;
 import mscalc.ratpack.RatPack.RAT;
 import mscalc.ratpack.RatPack.TYLOR;
 
+import static mscalc.ratpack.CalcErr.CALC_E_DOMAIN;
+import static mscalc.ratpack.Conv.destroyrat;
+import static mscalc.ratpack.Conv.i32tonum;
 import static mscalc.ratpack.Rat.*;
 import static mscalc.ratpack.RatPack.DUPNUM;
 import static mscalc.ratpack.RatPack.DUPRAT;
@@ -121,5 +125,161 @@ public interface Trans {
             break;
         }
         _sinrat(pa, precision);
+    }
+
+    //-----------------------------------------------------------------------------
+    //
+    //  FUNCTION: cosrat, _cosrat
+    //
+    //  ARGUMENTS:  x PRAT representation of number to take the cosine of
+    //
+    //  RETURN: cosine of x in PRAT form.
+    //
+    //  EXPLANATION: This uses Taylor series
+    //
+    //    n
+    //   ___    2j   j
+    //   \  ]  X   -1
+    //    \   ---------
+    //    /    (2j)!
+    //   /__]
+    //   j=0
+    //          or,
+    //    n
+    //   ___                                                 2
+    //   \  ]                                              -X
+    //    \   thisterm  ; where thisterm   = thisterm  * ---------
+    //    /           j                 j+1          j   (2j)*(2j+1)
+    //   /__]
+    //   j=0
+    //
+    //   thisterm  = 1 ;  and stop when thisterm < precision used.
+    //           0                              n
+    //
+    //-----------------------------------------------------------------------------
+    static void _cosrat(Ptr<RAT> px, uint radix, int precision)
+    {
+        TYLOR t = new TYLOR(px.deref(), precision);
+
+        t.pret.pp = i32tonum(1, radix);
+        t.pret.pq = i32tonum(1, radix);
+
+        t.thisterm = DUPRAT(t.pret);
+
+        t.n2 = i32tonum(0, radix);
+        t.xx.pp.sign *= -1;
+
+        do
+        {
+            t.NEXTTERM(t.xx, () -> {
+                t.n2 = t.INC(t.n2);
+                t.DIVNUM(t.n2);
+                t.n2 = t.INC(t.n2);
+                t.DIVNUM(t.n2);
+            }, precision);
+        } while (!t.thisterm.SMALL_ENOUGH_RAT(precision));
+
+        px.set(t.RESULT());
+
+        // Since *px might be epsilon above 1 or below -1, due to TRIMIT we need
+        // this trick here.
+        inbetween(px, rat_one, precision);
+
+        // Since *px might be epsilon near zero we must set it to zero.
+        if (rat_le(px.deref(), rat_smallest, precision) && rat_ge(px.deref(), rat_negsmallest, precision))
+        {
+            px.set(DUPRAT(rat_zero));
+        }
+    }
+
+    static void cosrat(Ptr<RAT> px, uint radix, int precision)
+    {
+        scale2pi(px, radix, precision);
+        _cosrat(px, radix, precision);
+    }
+
+    static void cosanglerat(Ptr<RAT> pa, AngleType angletype, uint radix, int precision)
+    {
+        scalerat(pa, angletype, radix, precision);
+        switch (angletype)
+        {
+            case AngleType.Degrees:
+                if (rat_gt(pa.deref(), rat_180, precision))
+                {
+                    Ptr<RAT> ptmp = new Ptr<>(DUPRAT(rat_360));
+                    subrat(ptmp, pa.deref(), precision);
+                    pa.set(ptmp.deref());
+                }
+                divrat(pa, rat_180, precision);
+                mulrat(pa, pi, precision);
+                break;
+            case AngleType.Gradians:
+                if (rat_gt(pa.deref(), rat_200, precision))
+                {
+                    Ptr<RAT> ptmp = new Ptr<>(DUPRAT(rat_400));
+                    subrat(ptmp, pa.deref(), precision);
+                    pa.set(ptmp.deref());
+                }
+                divrat(pa, rat_200, precision);
+                mulrat(pa, pi, precision);
+                break;
+        }
+        _cosrat(pa, radix, precision);
+    }
+
+    //-----------------------------------------------------------------------------
+    //
+    //  FUNCTION: tanrat, _tanrat
+    //
+    //  ARGUMENTS:  x PRAT representation of number to take the tangent of
+    //
+    //  RETURN: tan     of x in PRAT form.
+    //
+    //  EXPLANATION: This uses sinrat and cosrat
+    //
+    //-----------------------------------------------------------------------------
+    static void _tanrat(Ptr<RAT> px, uint radix, int precision)
+    {
+        Ptr<RAT> ptmp = new Ptr<>(DUPRAT(px.deref()));
+
+        _sinrat(px, precision);
+        _cosrat(ptmp, radix, precision);
+
+        if (zerrat(ptmp.deref()))
+        {
+            throw new ErrorCodeException(CALC_E_DOMAIN);
+        }
+        divrat(px, ptmp.deref(), precision);
+    }
+
+    static void tanrat(Ptr<RAT> px, uint radix, int precision)
+    {
+        scale2pi(px, radix, precision);
+        _tanrat(px, radix, precision);
+    }
+
+    static void tananglerat(Ptr<RAT> pa, AngleType angletype, uint radix, int precision)
+    {
+        scalerat(pa, angletype, radix, precision);
+        switch (angletype)
+        {
+            case AngleType.Degrees:
+                if (rat_gt(pa.deref(), rat_180, precision))
+                {
+                    subrat(pa, rat_180, precision);
+                }
+                divrat(pa, rat_180, precision);
+                mulrat(pa, pi, precision);
+                break;
+            case AngleType.Gradians:
+                if (rat_gt(pa.deref(), rat_200, precision))
+                {
+                    subrat(pa, rat_200, precision);
+                }
+                divrat(pa, rat_200, precision);
+                mulrat(pa, pi, precision);
+                break;
+        }
+        _tanrat(pa, radix, precision);
     }
 }
