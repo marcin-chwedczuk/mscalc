67345e0146d2e596bcd21dc2df6c104bc6e3e2a6
Marcin Chwedczuk <0xmarcin+gh@gmail.com>
2024-11-10 14:24:45 +0100

    Add ln

--- src/main/java/mscalc/ratpack/Exp.java
+++ src/main/java/mscalc/ratpack/Exp.java
@@ -3,15 +3,15 @@ package mscalc.ratpack;
 import mscalc.cpp.ErrorCodeException;
 import mscalc.cpp.Ptr;
 import mscalc.cpp.uint;
+import mscalc.ratpack.RatPack.NUMBER;
 import mscalc.ratpack.RatPack.RAT;
+import mscalc.ratpack.RatPack.TYLOR;
 
 import static mscalc.ratpack.CalcErr.CALC_E_DOMAIN;
 import static mscalc.ratpack.Conv.*;
 import static mscalc.ratpack.Num.addnum;
-import static mscalc.ratpack.Rat.mulrat;
-import static mscalc.ratpack.Rat.subrat;
-import static mscalc.ratpack.RatPack.BASEX;
-import static mscalc.ratpack.RatPack.DUPRAT;
+import static mscalc.ratpack.Rat.*;
+import static mscalc.ratpack.RatPack.*;
 import static mscalc.ratpack.Support.*;
 import static mscalc.ratpack.Support.Global.*;
 
@@ -40,7 +40,7 @@ public interface Exp {
     //-----------------------------------------------------------------------------
     static void _exprat(Ptr<RAT> px, uint radix, int precision)
     {
-        RatPack.TYLOR t = new RatPack.TYLOR(px.deref(), precision);
+        TYLOR t = new TYLOR(px.deref(), precision);
 
         t.pret.ppPtr(ppp -> addnum(ppp, num_one, radix));
         t.pret.pqPtr(ppq -> addnum(ppq, num_one, radix));
@@ -90,7 +90,152 @@ public interface Exp {
         }
     }
 
-    static void lograt(Ptr<RAT> px, int precision) {
-        System.out.println("Not implemented");
+    //-----------------------------------------------------------------------------
+    //
+    //  FUNCTION: lograt, _lograt
+    //
+    //  ARGUMENTS: x PRAT representation of number to logarithim
+    //
+    //  RETURN: log  of x in PRAT form.
+    //
+    //  EXPLANATION: This uses Taylor series
+    //
+    //    n
+    //   ___
+    //   \  ]                                             j*(1-X)
+    //    \   thisterm  ; where thisterm   = thisterm  * ---------
+    //    /           j                 j+1          j      j+1
+    //   /__]
+    //   j=0
+    //
+    //   thisterm  = X ;  and stop when thisterm < precision used.
+    //           0                              n
+    //
+    //   Number is scaled between one and e_to_one_half prior to taking the
+    //   log. This is to keep execution time from exploding.
+    //
+    //
+    //-----------------------------------------------------------------------------
+    static void _lograt(Ptr<RAT> px, int precision)
+    {
+        TYLOR t = new TYLOR(px.deref(), precision);
+
+        t.thisterm = createrat();
+
+        // sub one from x
+        px.deref().pq.sign *= -1;
+        px.deref().ppPtr(ppp -> addnum(ppp, px.deref().pq, BASEX));
+        px.deref().pq.sign *= -1;
+
+        t.pret = DUPRAT(px.deref());
+        t.thisterm = DUPRAT(px.deref());
+
+        t.n2 = i32tonum(1, BASEX);
+        px.deref().pp.sign *= -1;
+
+        do
+        {
+            t.NEXTTERM(px.deref(), () -> {
+                t.MULNUM(t.n2);
+                t.n2 = t.INC(t.n2);
+                t.DIVNUM(t.n2);
+            }, precision);
+
+            px.deref().TRIMTOP(precision);
+        } while (!t.thisterm.SMALL_ENOUGH_RAT(precision));
+
+        px.set( t.RESULT() );
     }
+
+    static void lograt(Ptr<RAT> px, int precision)
+    {
+        Ptr<RAT> pwr = new Ptr<>();    // pwr is the large scaling factor.
+        Ptr<RAT> offset = new Ptr<>(); // offset is the incremental scaling factor.
+
+        // Check for someone taking the log of zero or a negative number.
+        if (rat_le(px.deref(), rat_zero, precision))
+        {
+            throw new ErrorCodeException(CALC_E_DOMAIN);
+        }
+
+        // Get number > 1, for scaling
+        boolean fneglog = rat_lt(px.deref(), rat_one, precision);
+        if (fneglog)
+        {
+            NUMBER pnumtemp = px.deref().pp;
+            px.deref().pp = px.deref().pq;
+            px.deref().pq = pnumtemp;
+        }
+
+        // Scale the number within BASEX factor of 1, for the large scale.
+        // log(x*2^(BASEXPWR*k)) = BASEXPWR*k*log(2)+log(x)
+        if (px.deref().LOGRAT2() > 1)
+        {
+            final int intpwr = px.deref().LOGRAT2() - 1;
+            px.deref().pq.exp += intpwr;
+            pwr.set( i32torat(intpwr * BASEXPWR) );
+            mulrat(pwr, ln_two, precision);
+            // ln(x+e)-ln(x) looks close to e when x is close to one using some
+            // expansions.  This means we can trim past precision digits+1.
+            px.deref().TRIMTOP(precision);
+        }
+        else
+        {
+            pwr.set( DUPRAT(rat_zero) );
+        }
+
+        offset.set( DUPRAT(rat_zero) );
+        // Scale the number between 1 and e_to_one_half, for the small scale.
+        while (rat_gt(px.deref(), e_to_one_half, precision))
+        {
+            divrat(px, e_to_one_half, precision);
+            addrat(offset, rat_one, precision);
+        }
+
+        _lograt(px, precision);
+
+        // Add the large and small scaling factors, take into account
+        // small scaling was done in e_to_one_half chunks.
+        divrat(offset, rat_two, precision);
+        addrat(pwr, offset.deref(), precision);
+
+        // And add the resulting scaling factor to the answer.
+        addrat(px, pwr.deref(), precision);
+
+        trimit(px, precision);
+
+        // If number started out < 1 rescale answer to negative.
+        if (fneglog)
+        {
+            px.deref().pp.sign *= -1;
+        }
+    }
+
+    static void log10rat(Ptr<RAT> px, int precision)
+    {
+        lograt(px, precision);
+        divrat(px, ln_ten, precision);
+    }
+
+    //
+    // return if the given x is even number. The assumption here is its denominator is 1 and we are testing the numerator is
+    // even or not
+    static boolean IsEven(RAT x, uint radix, int precision)
+    {
+        Ptr<RAT> tmp = new Ptr<>();
+        boolean bRet = false;
+
+        tmp.set( DUPRAT(x) );
+        divrat(tmp, rat_two, precision);
+        fracrat(tmp, radix, precision);
+        addrat(tmp, tmp.deref(), precision);
+        subrat(tmp, rat_one, precision);
+        if (rat_lt(tmp.deref(), rat_zero, precision))
+        {
+            bRet = true;
+        }
+
+        return bRet;
+    }
+
 }
--- src/test/java/mscalc/ratpack/ExpTests.java
+++ src/test/java/mscalc/ratpack/ExpTests.java
@@ -52,9 +52,48 @@ public class ExpTests {
 
             String actual = Conv.RatToString(xRat, RatPack.NumberFormat.Float, RADIX_10, PRECISION);
 
-            // System.out.printf("exp(%s) = %s%n", x, expected);
+            // System.out.printf("exp(%s) = %s%n", x, actual);
             assertEquals(expected, actual);
         }
     }
 
+    @Test
+    public void ln_works() {
+        String[] data = new String[] {
+                // x,
+                "1", "0",
+                // Data from wolfram-alpha
+                "2.7182818284590452353602874713526624977572470936999595749669676277", "1",
+                "7.3890560989306502272", "2",
+                "26489122129.843472294", "24",
+                "6.9388714177584033016e+63", "147",
+
+                // Real x
+                "23.140677508263698368", "3.141592",
+                // ln(2)
+                "2","0.69314718055994530942",
+                // ln(100)
+                "100", "4.605170185988091368",
+
+                // Negative x
+                "0.3678794411714423215955237701614608674458111310317678345078368016", "-1",
+                // ln(1/10)
+                "0.1", "-2.302585092994045684"
+        };
+
+        for (int i = 0; i <= data.length - 2; i += 2) {
+            String x = data[i];
+            String expected = data[i+1];
+
+            Ptr<RAT> xRat = new Ptr<>(
+                    Conv.StringToRat(false, x, false, "0", RADIX_10, PRECISION)
+            );
+            Exp.lograt(xRat, PRECISION);
+
+            String actual = Conv.RatToString(xRat, RatPack.NumberFormat.Float, RADIX_10, PRECISION);
+
+            // System.out.printf("ln(%s) = %s%n", x, actual);
+            assertEquals(expected, actual);
+        }
+    }
 }
